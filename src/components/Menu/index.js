cc8fb7cf63db07d4de8f79e90e8642f725381a60
CaueMoalli <caue.moalli@gmail.com>
2020-07-29 14:45:01 -0300

    Adição de novos componentes

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "../../assets/img/logo.png";
 import "./Menu.css";
+import Button from "../Button";
 
 function Menu() {
   return (
@@ -8,7 +9,7 @@ function Menu() {
       <a href="/">
         <img className="Logo" src={Logo} alt="Logo da TechFlix" />
       </a>
-      <a className="ButtonLink" href="/">
+      <Button as="a" className="ButtonLink" href="/">
         Novo video
       </a>
     </nav>
