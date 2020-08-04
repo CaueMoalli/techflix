ffbe1de1000b39fbe25e93707f1d5bb8af840a86
CaueMoalli <caue.moalli@gmail.com>
2020-08-04 13:48:25 -0300

    Refactoring

--- src/repositories/categorias/index.js
+++ src/repositories/categorias/index.js
@@ -1,23 +1,29 @@
-import config from '../../config';
+import config from '../../config'
 
 const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
 
-async function getAll() {
-  const respostaDoServidor = await fetch(`${URL_CATEGORIES}`);
-  if (respostaDoServidor.ok) {
-    const resposta = await respostaDoServidor.json();
-    return resposta;
-  }
-  throw new Error('Não foi possível pegar os dados :(');
+function getAll() {
+  return fetch(`${URL_CATEGORIES}`)
+    .then(async (respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
+        return resposta;
+      }
+
+      throw new Error('Não foi possível pegar os dados :(');
+    });
 }
 
-async function getAllWithVideos() {
-  const respostaDoServidor = await fetch(`${URL_CATEGORIES}?_embed=videos`);
-  if (respostaDoServidor.ok) {
-    const resposta = await respostaDoServidor.json();
-    return resposta;
-  }
-  throw new Error('Não foi possível pegar os dados :(');
+function getAllWithVideos() {
+  return fetch(`${URL_CATEGORIES}?_embed=videos`)
+    .then(async (respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
+        return resposta;
+      }
+
+      throw new Error('Não foi possível pegar os dados :(');
+    });
 }
 
 export default {
